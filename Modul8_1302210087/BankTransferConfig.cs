d46af0a845c509575aa27892855f7771520d5ac4
Juliandri04 <94310353+Juliandri04@users.noreply.github.com>
2023-04-10 07:54:15 +0700

    aplikasibank transfer dengan runtime configuration

---
+++ Modul8_1302210087/BankTransferConfig.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using Newtonsoft.Json;
+
+namespace Modul8_1302210087
+{
+    internal class BankTransferConfig
+    {
+        public void ReadJSON()
+        {
+            string json = File.ReadAllText(@"C:\Users\LENOVO\source\repos\Modul8_1302210087\Modul8_1302210087\bank_transfer_config.json");
+            dynamic jsonGif = JsonConvert.DeserializeObject(json);
+
+            if (jsonGif.lang = "en")
+            {
+                Console.WriteLine("Please insert the amount of money to transfer: ");
+            }
+            else
+            {
+                Console.WriteLine("Masukkan jumlah uang yang akan di-transfer:");
+            }
+
+            if (jsonGif.transfer.threshold <= 25000000)
+            {
+            
+            }
+        }
+    }
+}
---
+++ Modul8_1302210087/Program.cs
@@ -0,0 +1,3 @@
+﻿using Modul8_1302210087;
+BankTransferConfig x = new BankTransferConfig();
+x.ReadJSON();
